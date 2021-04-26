7e2add7cf262c3c0f6e8e14536376cc894c0864b
Saravanancs <39839870+Saravanancs@users.noreply.github.com>
2021-04-25 21:56:29 -0400

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -2,6 +2,7 @@ package hello;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 import org.junit.Test;
 
