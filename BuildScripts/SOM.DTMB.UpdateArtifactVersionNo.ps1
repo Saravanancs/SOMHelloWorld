f0fd39e968ba7430b72b9ab5c9ece2451720be83
Saravanancs <39839870+Saravanancs@users.noreply.github.com>
2021-04-28 15:28:21 -0400

    Add files via upload

---
+++ BuildScripts/SOM.DTMB.UpdateArtifactVersionNo.ps1
@@ -0,0 +1,19 @@
+﻿#####################################################################
+# Copyright											
+# Update POM XML artifact version no with Build Number PowerShell Script.
+# Copyright (c) 2021, State of Michigan - DTMB Agency Services. All rights
+# reserves.`n.
+######################################################################
+Param 
+(   
+    [Parameter(Mandatory=$false)]
+    [string] $BuildSourceDirectory = $Env:BUILD_SOURCESDIRECTORY,
+
+    [Parameter(Mandatory=$false)]
+    [string] $Verbose = $false 
+  
+) 
+
+Write-Verbose "Enter Update POM XML artifact version no activity execution, please wait.."
+Write-Host ("Source Directory: {0}"-f $BuildSourceDirectory)
+Write-Host ("Source Directory: {1}"-f $(Build.SourcesDirectory))
