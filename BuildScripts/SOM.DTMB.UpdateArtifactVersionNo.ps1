1b49b8ac71f1fe31fda9205449cff323150cf0fb
Saravanancs <39839870+Saravanancs@users.noreply.github.com>
2021-04-28 16:05:41 -0400

    Update SOM.DTMB.UpdateArtifactVersionNo.ps1

--- BuildScripts/SOM.DTMB.UpdateArtifactVersionNo.ps1
+++ BuildScripts/SOM.DTMB.UpdateArtifactVersionNo.ps1
@@ -7,13 +7,13 @@
 Param 
 (   
     [Parameter(Mandatory=$false)]
-    [string] $BuildSourceDirectory = $Env:BUILD_SOURCESDIRECTORY,
+    [string] $BuildSourceDirectory = $Env:BUILD_SOURCESDIRECTORY
 
-    [Parameter(Mandatory=$false)]
-    [string] $verbose = $false 
+    #[Parameter(Mandatory=$false)]
+    #[string] $verbose = $false 
   
 ) 
 
-Write-Verbose "Enter Update POM XML artifact version no activity execution, please wait.."
+#Write-Verbose "Enter Update POM XML artifact version no activity execution, please wait.."
 Write-Host ("Source Directory: {0}"-f $BuildSourceDirectory)
 Write-Host ("Source Directory: {1}"-f $(Build.SourcesDirectory))
